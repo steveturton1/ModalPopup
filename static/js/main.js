214781bbffb4cfc0d1661e7c33bd92ef6ce68aef
steveturton1 <steve.turton@visualretailing.com>
2015-08-04 13:53:17 +0100

    x

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,35 @@
  * Created by Steve on 03/08/2015.
  */
 
-function test() {
-    var x;
-    x = 2;
+function showModal() {
+
+    //$(".content").addClass("inactive");
+    //$(".content").fadeTo('fast', 0.1, function() {
+    //    $(".modal-overlay").css("visibility","visible");
+    //    $(".modal-dialog").css("visibility","visible");
+    //});
+
+    $(".content").fadeTo('fast', 0.1);
+    $(".modal-overlay").css("visibility","visible");
+    $(".modal-dialog").css("visibility","visible");
 };
+
+function hideModal() {
+    //$(".content").removeClass("inactive");
+    $(".content").fadeTo('fast', 1);
+    $(".modal-overlay").css("visibility","hidden");
+    $(".modal-dialog").css("visibility","hidden");
+};
+
+function pmo_animate() {
+    /*$(".modal-dialog").animate({
+            height: '+=50px',
+            width: '+=50px'
+        }, 'slow');
+
+    $(".modal-dialog").animate({
+            height: '-=50px',
+            width: '-=50px'
+        }, 'slow');
+    */
+}
